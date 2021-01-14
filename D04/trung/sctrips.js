3dad446018d1a309ef5bfa442b949cee98910c43
trung <trung.nt4996@sinhvien.hoasen.edu.vn>
2021-01-14 16:23:25 +0700

    D04

---
+++ D04/trung/sctrips.js
@@ -0,0 +1,8 @@
+let x = ["khong" , "mot" , "hai" , "ba" , "bon" , "nam" , "sau" , "bay" , "tam" , "chin" , "muoi"]
+let y = ["chuc" , "nghin" , "tram" , " chuc nghin" , "tram nghin"]
+let tram ;
+let nghin;
+let donvi;
+let kq=""; 
+nghin=parseInt(sotien/1000);
+tram = parseInt(sotien%1000)/
