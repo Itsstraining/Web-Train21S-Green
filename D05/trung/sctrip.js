b6160ebad2ff1acc58806652c75d3840a3d47bc8
trung <trung.nt4996@sinhvien.hoasen.edu.vn>
2021-01-15 16:08:08 +0700

    D05

---
+++ D05/trung/sctrip.js
@@ -0,0 +1,88 @@
+// let database = [
+//     {
+//         name: "Thịt ba chỉ",
+//         price:5000,
+//         img:"https://lh3.googleusercontent.com/uucnWcE5ReETrOdxta4K9AG_py5cfCfvbVtzMXbY5GsL0uoFxQMNCdkUDQuTNpaF3c8feHiuM4AtUtxK4Js=w185",
+    
+//     },
+//     { 
+//         name: "Ba chỉ bò úc",
+//         price:4000,
+//         img:"https://lh3.googleusercontent.com/uucnWcE5ReETrOdxta4K9AG_py5cfCfvbVtzMXbY5GsL0uoFxQMNCdkUDQuTNpaF3c8feHiuM4AtUtxK4Js=w185",
+        
+//     },
+//     { 
+//         name: "Dầu ăn",
+//         price:1000,
+//         img:"https://lh3.googleusercontent.com/uucnWcE5ReETrOdxta4K9AG_py5cfCfvbVtzMXbY5GsL0uoFxQMNCdkUDQuTNpaF3c8feHiuM4AtUtxK4Js=w185",
+        
+//     },
+//     { 
+//         name: "Hàu sữa biển",
+//         price:5000,
+//         img:"https://lh3.googleusercontent.com/uucnWcE5ReETrOdxta4K9AG_py5cfCfvbVtzMXbY5GsL0uoFxQMNCdkUDQuTNpaF3c8feHiuM4AtUtxK4Js=w185",
+        
+//     },
+//     { 
+//         name: "Chả cá",
+//         price:4000,
+//         img:"https://lh3.googleusercontent.com/uucnWcE5ReETrOdxta4K9AG_py5cfCfvbVtzMXbY5GsL0uoFxQMNCdkUDQuTNpaF3c8feHiuM4AtUtxK4Js=w185",
+        
+//     },
+//     { 
+//         name: "Ngêu",
+//         price:1000,
+//         img:"https://lh3.googleusercontent.com/uucnWcE5ReETrOdxta4K9AG_py5cfCfvbVtzMXbY5GsL0uoFxQMNCdkUDQuTNpaF3c8feHiuM4AtUtxK4Js=w185",
+        
+//     },
+//     { 
+//         name: "Bắp cải",
+//         price:5000,
+//         img:"https://lh3.googleusercontent.com/uucnWcE5ReETrOdxta4K9AG_py5cfCfvbVtzMXbY5GsL0uoFxQMNCdkUDQuTNpaF3c8feHiuM4AtUtxK4Js=w185",
+        
+//     },
+//     { 
+//         name: "Nấm",
+//         price:4000,
+//         img:"https://lh3.googleusercontent.com/uucnWcE5ReETrOdxta4K9AG_py5cfCfvbVtzMXbY5GsL0uoFxQMNCdkUDQuTNpaF3c8feHiuM4AtUtxK4Js=w185",
+        
+//     },
+//     { 
+//         name: "Bí đỏ",
+//         price:5000,
+//         img:"https://lh3.googleusercontent.com/uucnWcE5ReETrOdxta4K9AG_py5cfCfvbVtzMXbY5GsL0uoFxQMNCdkUDQuTNpaF3c8feHiuM4AtUtxK4Js=w185",
+        
+//     },
+//     { 
+//         name: "Rau muống",
+//         price:5000,
+//         img:"https://lh3.googleusercontent.com/uucnWcE5ReETrOdxta4K9AG_py5cfCfvbVtzMXbY5GsL0uoFxQMNCdkUDQuTNpaF3c8feHiuM4AtUtxK4Js=w185",
+        
+//     },
+// ]
+
+
+// let total = 0;
+// let cart = [];
+// function thanhtoan() {
+//     for (let i = 0; i < cart.length; i++) {
+//         total = total + cart[i].price;
+//     }
+//     let tagtotal 
+
+
+
+// let but = document.getElementsByClassName("btnbuy");
+// but.onclick= ()=>{
+//     alert("helloworld");
+// };
+function but()
+{
+    let but = document.getElementById("cart");
+    but.style.color = "blue";
+
+}
+function test()
+{
+    let 
+}
