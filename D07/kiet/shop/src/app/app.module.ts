e47d8ab4022323060a3a92e3dd502b583be30dc8
seb <sebastian@seventh.ai>
2021-01-19 16:37:51 +0700

    nop bai

---
+++ D07/kiet/shop/src/app/app.module.ts
@@ -0,0 +1,24 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { ItemComponent } from './item/item.component';
+import { ItemListComponent } from './item-list/item-list.component';
+import { NavComponent } from './nav/nav.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    ItemComponent,
+    ItemListComponent,
+    NavComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ D07/kiet/shop/src/app/item/item.component.ts
@@ -0,0 +1,30 @@
+import { Component, Input, OnInit } from '@angular/core';
+import { Item } from '../model/item.model';
+
+@Component({
+  selector: 'app-item',
+  templateUrl: './item.component.html',
+  styleUrls: ['./item.component.scss']
+})
+export class ItemComponent implements OnInit {
+  public count = 1
+
+  constructor() { }
+  
+  @Input()
+  item:Item;
+  
+  
+
+  ngOnInit(): void {
+  }
+  public increase(){
+
+    this.count += 1;
+  }
+
+  public decrease(){
+    this.count -= this.count == 1?0:1;
+  }
+
+}
