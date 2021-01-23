e8b0d4efe1fe753316df254161c6fe2d6f381103
art3mis145 <art3mis145@gmail.com>
2021-01-23 16:51:05 +0700

    thuyettrinh

--- Hackathon2/src/app/app-routing.module.ts
+++ Hackathon2/src/app/app-routing.module.ts
@@ -1,10 +1,17 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [{ path: 'excel', loadChildren: () => import('./pages/excel/excel.module').then(m => m.ExcelModule) }];
+const routes: Routes = [
+  {
+    path: '**',
+    pathMatch: 'full',
+    loadChildren: () =>
+      import('./pages/excel/excel.module').then((m) => m.ExcelModule),
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
--- Hackathon2/src/app/app.module.ts
+++ Hackathon2/src/app/app.module.ts
@@ -2,10 +2,11 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { CommonModule } from '@angular/common';
+import { AppRoutingModule } from './app-routing.module';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [CommonModule, BrowserModule],
+  imports: [CommonModule, BrowserModule, AppRoutingModule],
   providers: [],
   bootstrap: [AppComponent],
 })
